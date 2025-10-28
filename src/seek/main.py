d307948a1412bd77ba29c2631c381a164bc69cfe
colmf1 <colmfearon@hotmail.com>
2025-10-28 23:12:26 +0000

    updated todo

--- src/seek/main.py
+++ src/seek/main.py
@@ -7,16 +7,17 @@ from dotenv import load_dotenv
 import re
 import logging
 
+
 def main():
     logging.basicConfig(
-        filename='app.log',
+        filename="app.log",
         level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
+        format="%(asctime)s - %(levelname)s - %(message)s",
     )
 
     config_dir = os.path.expanduser("~/.config/seek")
     env_path = os.path.join(config_dir, ".env")
-    output_dir = os.path.join(config_dir, 'responses')
+    output_dir = os.path.join(config_dir, "responses")
     load_dotenv(env_path)
 
     result = subprocess.run(
@@ -34,7 +35,7 @@ def main():
     exit() if result.returncode != 0 else None
     question = result.stdout.strip()
     timestamp = datetime.now().strftime("%Y%m%d%H%M")
-    logging.info(f'User Input \n{question}')
+    logging.info(f"User Input \n{question}")
 
     prompt = (
         "You are a helpful assistant."
@@ -47,28 +48,20 @@ def main():
 
     provider = os.getenv("AI_PROVIDER")
     api_key = os.getenv("AI_API_KEY")
-    
+
     if provider == "anthropic":
-        llm = ChatAnthropic(
-            model = "claude-sonnet-4-5-20250929",
-            api_key=api_key
-        )
+        llm = ChatAnthropic(model="claude-sonnet-4-5-20250929", api_key=api_key)
     elif provider == "deepseek":
         llm = ChatOpenAI(
-            model = 'deepseek-chat',
-            api_key=api_key,
-            base_url='https://api.deepseek.com'
+            model="deepseek-chat", api_key=api_key, base_url="https://api.deepseek.com"
         )
     else:
-        llm = ChatOpenAI(
-            model="gpt-4",
-            api_key=api_key
-        )
+        llm = ChatOpenAI(model="gpt-4", api_key=api_key)
 
     try:
         response = llm.invoke(prompt).content
 
-        logging.info(f'response \n{response}')
+        logging.info(f"response \n{response}")
         match = re.search(pattern=r"\{([^{}]+)\}", string=response)
         if match:
             title = match.group(1)
@@ -76,12 +69,12 @@ def main():
         else:
             output_file = os.path.join(output_dir, f"{timestamp}.md")
 
-        logging.info(f'Output Filenamme \n{output_file}')
+        logging.info(f"Output Filenamme \n{output_file}")
 
         with open(output_file, "w") as f:
             f.write(f"# Question\n\n{question}\n\n# Response\n\n{response}")
         subprocess.run(["kitty", "nvim", output_file])
-        
+
     except Exception as e:
         print(f"Error: {e}")
 
--- todo.md
+++ todo.md
@@ -1,21 +1,17 @@
 ### TODO:: 
 
+There's a lot we can do in uv 
+I'll need 
+- shell script to run it should be fine 
+- cli app with typer
+- need to compile into single binary with uv build 
+
 Currently the nvim output isn't opening correctly 
 - Could be due to filename that comes from LLM - it's been looking sus 
 - Could be due to being unable to open correct file 
+- Should be fixed by shell script 
 
-
-I also think I need to add support for different terminals, or at least add kitty to install.sh 
-
-Eventually I'd like to be able to switch providers - but I'm not sure if zenity is the tool for that 
-
-I'd also like it to work as a command line tool 
 - Seek set changes provider
 - Seek install runs install script
 - Seek uninstall removes install script 
-
-
-I also need to do something more smart about environment variables - I never seem to have any... 
-
-
 
